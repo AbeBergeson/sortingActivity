5850b0924c3499e552786a811623dfe90b979992
AbeBergeson <abe46077@gmail.com>
2024-04-25 11:22:14 -0400

    Uploaded April 2024 (Test File)

---
+++ src/sort.java
@@ -0,0 +1,13 @@
+public class sort {
+    public int [] sortingArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (array[j]>array[j+1]) {
+
+                }
+            }
+        }
+        return array;
+    }
+}
